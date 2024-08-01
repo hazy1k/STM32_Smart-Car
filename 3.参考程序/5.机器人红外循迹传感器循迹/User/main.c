e81f17ca894f2c707518c7ec354059aaa3cc1a29
hazy1k <160259482+hazy1k@users.noreply.github.com>
2024-08-01 13:28:22 +0800

    code

---
+++ 3.参考程序/5.机器人红外循迹传感器循迹/User/main.c
@@ -0,0 +1,35 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+#include "robot.h"
+#include "Irtracking.h"
+#include "Key.h"
+
+uint8_t i;
+
+int main(void)
+{
+	robot_Init();        // 机器人初始化
+	Key_Init();          // 按键初始化
+	Irtracking_Init();   // 红外避障传感器初始化
+	while(Key_GetNum() == 0);  //等待按键按下
+	while (1)
+	{
+	
+			if(Left_Irtracking_Get() == 0 && Right_Irtracking_Get() == 0)
+			{
+				makerobo_run(70,0);   // 前进
+			}
+			else if(Left_Irtracking_Get() == 1 && Right_Irtracking_Get() == 0) //左侧压线
+			{
+				makerobo_Left(70,0);    //左转修正
+			}
+			else if(Left_Irtracking_Get() == 0 && Right_Irtracking_Get() == 1) // 右侧压线
+			{
+				makerobo_Right(70,0); // 右转修正
+			}			
+			else if(Left_Irtracking_Get() == 1 && Right_Irtracking_Get() == 1) // 全部压线，停止
+			{
+       makerobo_brake(0);
+			}
+	}
+}
