123569d8dd1148d6ac19a210401e20c9fb5b1e75
hazy1k <160259482+hazy1k@users.noreply.github.com>
2024-08-02 10:25:15 +0800

    拓展资料

---
+++ 8.stm32有关学习资料/2.EMWIN学习资料/STemWin源码/Project/STM324xG-EVAL/RTOS/User/bsp.c
@@ -0,0 +1,109 @@
+/**
+  ******************************************************************************
+  * @file    bsp.c
+  * @author  MCD Application Team
+  * @version V1.1.2
+  * @date    28-January-2014
+  * @brief   This file provides targets hardware configuration 
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2014 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "bsp.h"
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+/* Private function prototypes -----------------------------------------------*/
+static uint32_t BSP_TSC_Init(void);
+/* Private functions ---------------------------------------------------------*/
+
+/**
+* @brief  Initializes the target hardware.
+* @param  None
+* @retval None
+*/
+uint32_t BSP_Init (void)
+{
+  /* Initialize the LEDs */
+  STM_EVAL_LEDInit(LED1);
+  STM_EVAL_LEDInit(LED2);
+  STM_EVAL_LEDInit(LED3);
+  STM_EVAL_LEDInit(LED4);  
+
+  BSP_TSC_Init();
+  
+  STM324xG_LCD_Init();
+
+  /* Initialize the SRAM */
+  SRAM_Init();
+  
+  /* Initialize the Ethernet BSP */
+  ETH_BSP_Config();
+  
+  /* Enable the CRC Module */
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_CRC, ENABLE);  
+  return 0;
+}
+
+
+/**
+  * @brief  Initializes the IO Expander registers.
+  * @param  None
+  * @retval - 0: if all initializations are OK.
+*/
+static uint32_t BSP_TSC_Init(void)
+{
+  portENTER_CRITICAL();  
+  IOE_Config();
+  portEXIT_CRITICAL();    
+  return 0; /* Configuration is OK */
+}
+
+/**
+  * @brief  Provide the GUI with current state of the touch screen
+  * @param  None
+  * @retval None
+  */
+void BSP_Pointer_Update(void)
+{
+  GUI_PID_STATE TS_State;
+  TS_STATE  *ts;
+  
+  portENTER_CRITICAL();
+  ts = IOE_TS_GetState();
+  portEXIT_CRITICAL ();
+  
+  TS_State.x = ts->X;
+  TS_State.y = ts->Y;
+  TS_State.Pressed = (ts->TouchDetected == 0x80);
+  TS_State.Layer = 0;
+  
+  if((TS_State.x >= 0 ) && 
+     (TS_State.x < LCD_GetXSize())&&
+     (TS_State.y >= 0 ) && 
+     (TS_State.y < LCD_GetYSize()))
+    
+  {
+    GUI_TOUCH_StoreStateEx(&TS_State);  
+  }  
+}
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ 8.stm32有关学习资料/2.EMWIN学习资料/STemWin源码/Project/STM324xG-EVAL/RTOS/User/main.c
@@ -0,0 +1,173 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  MCD Application Team
+  * @version V1.1.2
+  * @date    28-January-2014
+  * @brief   This file provides main program functions
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2014 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "bsp.h"
+#include "timers.h"
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+#define Background_Task_PRIO    ( tskIDLE_PRIORITY  + 9 )
+#define Background_Task_STACK   ( 512 )
+
+#define Demo_Task_PRIO          ( tskIDLE_PRIORITY  + 8 )
+#define Demo_Task_STACK         ( 512 )
+
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+xTaskHandle                   Task_Handle;
+xTaskHandle                   Demo_Handle;
+xTimerHandle                  TouchScreenTimer;
+
+/* Private function prototypes -----------------------------------------------*/
+static void Background_Task(void * pvParameters);
+static void Demo_Task(void * pvParameters);
+static void vTimerCallback( xTimerHandle pxTimer );
+
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief  Main program.
+  * @param  None
+  * @retval None
+  */ 
+int main(void)
+{
+  NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
+  
+  /* Create background task */
+  xTaskCreate(Background_Task,
+              (signed char const*)"BK_GND",
+              Background_Task_STACK,
+              NULL,
+              Background_Task_PRIO,
+              &Task_Handle);
+
+    /* Create demo task */
+  xTaskCreate(Demo_Task,
+              (signed char const*)"GUI_DEMO",
+              Demo_Task_STACK,
+              NULL,
+              Demo_Task_PRIO,
+              &Demo_Handle);
+  
+  /* Launch Touchscreen Timer */
+  TouchScreenTimer = xTimerCreate ("Timer", 50, pdTRUE, ( void * ) 1, vTimerCallback );
+  
+  if( TouchScreenTimer != NULL )
+  {
+    if( xTimerStart( TouchScreenTimer, 0 ) != pdPASS )
+    {
+      /* The timer could not be set into the Active state. */
+    }
+  }
+  
+  /* Start the FreeRTOS scheduler */
+  vTaskStartScheduler();
+}
+
+/**
+  * @brief  Background task
+  * @param  pvParameters not used
+  * @retval None
+  */
+static void Background_Task(void * pvParameters)
+{  
+  /* Initilize the BSP layer */
+  BSP_Init();
+  
+  /* Activate the use of memory device feature */
+  WM_SetCreateFlags(WM_CF_MEMDEV);
+  
+  /* Init the STemWin GUI Library */
+  GUI_Init();
+  
+  /* Run the background task */
+  while (1)
+  {
+      /* toggle LED1..4 each 100ms */
+      STM_EVAL_LEDToggle(LED1);
+      STM_EVAL_LEDToggle(LED2);
+      STM_EVAL_LEDToggle(LED3);
+      STM_EVAL_LEDToggle(LED4);
+
+	  GUI_Delay(250);
+  }
+}
+
+/**
+  * @brief  Demonstration task
+  * @param  pvParameters not used
+  * @retval None
+  */
+static void Demo_Task(void * pvParameters)
+{
+  /* Start Demo */
+  GUIDEMO_Main();
+}
+
+/**
+  * @brief  Timer callbacsk
+  * @param  pxTimer 
+  * @retval None
+  */
+static void vTimerCallback( xTimerHandle pxTimer )
+{
+   BSP_Pointer_Update();
+}
+
+/**
+  * @brief  Error callback function
+  * @param  None
+  * @retval None
+  */
+void vApplicationMallocFailedHook( void )
+{
+  while (1)
+  {}
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *   where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {}
+}
+#endif
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
