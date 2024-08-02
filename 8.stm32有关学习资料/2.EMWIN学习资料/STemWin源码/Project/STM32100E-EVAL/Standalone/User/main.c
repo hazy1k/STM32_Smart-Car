7a1bd75d7896c5a8a36fcc0a06bff20eccd02ed7
hazy1k <160259482+hazy1k@users.noreply.github.com>
2024-08-02 10:23:58 +0800

    拓展资料

---
+++ 8.stm32有关学习资料/2.EMWIN学习资料/STemWin源码/Project/STM32100E-EVAL/Standalone/User/main.c
@@ -0,0 +1,114 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  MCD Application Team
+  * @version V1.1.2
+  * @date    28-January-2014
+  * @brief   This file provides main program functions
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2014 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "bsp.h"
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+uint8_t GUI_Initialized   = 0;
+/* Private function prototypes -----------------------------------------------*/
+/* Private functions ---------------------------------------------------------*/
+/**
+  * @brief  Main program.
+  * @param  None
+  * @retval None
+  */ 
+int main(void)
+{
+
+  NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
+  
+  /* Initilize the BSP layer */
+  BSP_Init();
+  
+  /* Activate the use of memory device feature */
+  WM_SetCreateFlags(WM_CF_MEMDEV);
+  
+  /* Init the STemWin GUI Library */
+  GUI_Init();
+  
+  GUI_Initialized = 1;
+
+  /* Start Demo */
+  GUIDEMO_Main();
+ }
+
+/**
+* @brief  BSP_Background.
+* @param  None
+* @retval None
+*/ 
+void BSP_Background (void)
+{
+  static uint32_t LED_ticks = 0;
+  static uint32_t POINTER_ticks = 0;
+  
+  if ( LED_ticks++ > 100 )
+  {
+    LED_ticks = 0;
+    
+    /* toggle LED1..4 each 100ms */
+    STM_EVAL_LEDToggle(LED1);
+    STM_EVAL_LEDToggle(LED2);
+    STM_EVAL_LEDToggle(LED3);
+    STM_EVAL_LEDToggle(LED4);
+  }
+
+  if ( POINTER_ticks++ > 50 )
+  {
+    POINTER_ticks = 0;
+    /* Capture input event and updade cursor */
+    if(GUI_Initialized == 1)
+    {
+       BSP_Pointer_Update();
+    }
+  }
+}  
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *   where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {}
+}
+#endif
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
---
+++ 8.stm32有关学习资料/2.EMWIN学习资料/STemWin源码/Project/STM32373C-EVAL/RTOS/User/main.c
@@ -0,0 +1,163 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  MCD Application Team
+  * @version V1.1.2
+  * @date    28-January-2014
+  * @brief   This file provides main program functions
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; COPYRIGHT 2014 STMicroelectronics</center></h2>
+  *
+  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
+  * You may not use this file except in compliance with the License.
+  * You may obtain a copy of the License at:
+  *
+  *        http://www.st.com/software_license_agreement_liberty_v2
+  *
+  * Unless required by applicable law or agreed to in writing, software 
+  * distributed under the License is distributed on an "AS IS" BASIS, 
+  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  * See the License for the specific language governing permissions and
+  * limitations under the License.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "bsp.h"
+
+/* Private typedef -----------------------------------------------------------*/
+/* Private define ------------------------------------------------------------*/
+#define Background_Task_PRIO    ( tskIDLE_PRIORITY  + 10 )
+#define Background_Task_STACK   ( 512 )
+
+#define Demo_Task_PRIO          ( tskIDLE_PRIORITY  + 9 )
+#define Demo_Task_STACK         (512)//( 384 )
+
+
+/* Private macro -------------------------------------------------------------*/
+/* Private variables ---------------------------------------------------------*/
+xTaskHandle                   Task_Handle;
+xTaskHandle                   Demo_Handle;
+
+/* Private function prototypes -----------------------------------------------*/
+static void Background_Task(void * pvParameters);
+static void Demo_Task(void * pvParameters);
+/* Private functions ---------------------------------------------------------*/
+
+/**
+  * @brief  Main program.
+  * @param  None
+  * @retval None
+  */ 
+int main(void)
+{
+
+  NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
+  
+  /* Create background task */
+  xTaskCreate(Background_Task,
+              (signed char const*)"BK_GND",
+              Background_Task_STACK,
+              NULL,
+              Background_Task_PRIO,
+              &Task_Handle);
+
+    /* Create demo task */
+  xTaskCreate(Demo_Task,
+              (signed char const*)"GUI_DEMO",
+              Demo_Task_STACK,
+              NULL,
+              Demo_Task_PRIO,
+              &Demo_Handle);
+  
+  /* Start the FreeRTOS scheduler */
+  vTaskStartScheduler();
+}
+
+
+/**
+  * @brief  Background task
+  * @param  pvParameters not used
+  * @retval None
+  */
+static void Background_Task(void * pvParameters)
+{
+  static uint32_t ticks = 0;
+  
+  /* Initilize the BSP layer */
+  BSP_Init();
+  
+  /* Activate the use of memory device feature */
+  WM_SetCreateFlags(WM_CF_MEMDEV);
+  
+  /* Init the STemWin GUI Library */
+  GUI_Init();
+  
+ 
+  /* Run the background task */
+  while (1)
+  {
+    if ( ticks++ > 10 )
+    {
+      ticks = 0;
+
+      /* toggle LED1..4 each 100ms */
+      STM_EVAL_LEDToggle(LED1);
+      STM_EVAL_LEDToggle(LED2);
+      STM_EVAL_LEDToggle(LED3);
+      STM_EVAL_LEDToggle(LED4);
+    }
+    /* Capture input event and updade cursor */
+    BSP_Pointer_Update();
+  
+    /* This task is handled periodically, each 10 mS */
+    GUI_Delay(10);
+  }
+}
+
+
+/**
+  * @brief  Demonstration task
+  * @param  pvParameters not used
+  * @retval None
+  */
+static void Demo_Task(void * pvParameters)
+{
+  /* Start Demo */
+  GUIDEMO_Main();
+}
+
+/**
+  * @brief  Error callback function
+  * @param  None
+  * @retval None
+  */
+void vApplicationMallocFailedHook( void )
+{
+  while (1)
+  {}
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *   where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t* file, uint32_t line)
+{
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+
+  /* Infinite loop */
+  while (1)
+  {}
+}
+#endif
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
